47bf6ef88cab21bae7b756c1a955dbb5042c8b1f
Zach Frohardt <zfrohardt@gmail.com>
2021-05-07 15:48:13 -0700

    fixed indentation in App.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import Robot from './components/Robot';
 import 'semantic-ui-css/semantic.min.css';
+import { Card } from 'semantic-ui-react';
 
 const attributes = [
-  {
-    name: "First",
-    details: "I am first",
-  },
-  {
-    name: "Second",
-    details: "I am second",
-  },
-  {
-    name: "Third",
-    details: "I am third",
-  }
+    {
+        name: "Quick Fire",
+        details: "I am first",
+    },
+    {
+        name: "Splash Damage",
+        details: "I am second",
+    },
+    {
+        name: "Kamikaze",
+        details: "I am third",
+    }
 ]
 
 function App() {
-  return (
-    <div className="App">
-      <Robot img={"https://robohash.org/VJ9.png?set=set1"} name={"Wall-E"} modelNumber={42} description={"They call me a killer robot, but I just want to find love"} attributes={attributes} />
-      <Robot />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Card.Group itemsPerRow={3} >
+                <Robot img={"https://robohash.org/VJ9.png?set=set1"} name={"Wall-E"} modelNumber={42} description={"They call me a killer robot, but I just want to find love"} attributes={attributes} />
+                <Robot />
+                <Robot />
+            </Card.Group>
+        </div>
+    );
 }
 
 export default App;
