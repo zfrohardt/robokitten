e072b888814380c3da7eea2b5c47d1ccdc906216
Zach Frohardt <zfrohardt@gmail.com>
2021-05-07 14:54:55 -0700

    added accordian for robot attributes

--- src/components/Robot.js
+++ src/components/Robot.js
@@ -1,8 +1,9 @@
 // Display component for a Robot
-import React from 'react';
-import {Card, Image} from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Accordion, Card, Icon, Image} from 'semantic-ui-react';
 
 const Robot = props => {
+    const [activeIndex, setIndex] = useState(-1);
     return (
         <Card>
             <Image src={props.img} wrapped ui={false} />
@@ -13,9 +14,36 @@ const Robot = props => {
             </Card.Content>
             <Card.Content extra>
                 We would put all of the bots attributes here
+                <Accordion fluid styled>
+                    <Accordion.Title active={activeIndex === 0} index={0} onClick={() => handleClick(setIndex, activeIndex, 0)} >
+                        <Icon name="dropdown" />
+                        Attribute 1
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 0} >
+                        Details for Attribute 1
+                    </Accordion.Content>
+                    <Accordion.Title active={activeIndex === 1} index={1} onClick={() => handleClick(setIndex, activeIndex, 1)} >
+                        <Icon name="dropdown" />
+                        Attribute 2
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 1} >
+                        Details for Attribute 2
+                    </Accordion.Content>
+                    <Accordion.Title active={activeIndex === 2} index={2} onClick={() => handleClick(setIndex, activeIndex, 2)} >
+                        <Icon name="dropdown" />
+                        Attribute 3
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 2} >
+                        Details for Attribute 3
+                    </Accordion.Content>
+                </Accordion>
             </Card.Content>
         </Card>
     );
+}
+
+const handleClick = (setIndex, activeIndex, index) => {
+    setIndex((activeIndex === index)? -1 : index);
 }
 
 Robot.defaultProps = {
