c5bec365d6812f79269d5303553c666a51518ed9
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 16:41:35 -0700

    added component for displaying game stats

---
+++ src/components/Stats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats = props => {
+    return (
+        <div>
+            This is where we can record our stats
+        </div>
+    );
+}
+
+export default Stats;
