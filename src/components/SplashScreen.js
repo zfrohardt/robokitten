87229f3c2a2d07a014ddc8c7feb55977dbba18a2
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 16:43:29 -0700

    added functionality to make links work in the SplashScreen

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,15 +1,14 @@
-import React, {Link} from 'react';
+import React from 'react';
 import {Button, Container, Header} from 'semantic-ui-react';
 
 const SplashScreen = props => (
     <Container text>
         <Header as='h1'>Welcome to our game!</Header>
         <Button.Group vertical labeled icon>
-            <Button icon="play" content="Exterminate the Kittens!" color="green" /> {/* Start the game */}
-            <Button icon="factory" content="Examine the Troops!" color="yellow" /> {/* Look at the list of robots */}
-            <Button icon="hand victory" content="Admire our Victories!" color="yellow" /> {/* Look at high scores */}
+            <Button as='a' href='/play' icon="play" content="Exterminate the Kittens!" color="green" /> {/* Start the game */}
+            <Button as='a' href='/troops' icon="factory" content="Examine the Troops!" color="yellow" /> {/* Look at the list of robots */}
+            <Button as='a' href='/statistics' icon="hand victory" content="Admire our Victories!" color="yellow" /> {/* Look at high scores */}
         </Button.Group>
-        <Link href="/test" />
     </Container>
 )
 
