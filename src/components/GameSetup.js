26475c37c75277087c467911b52cee2d2f6901c3
Zach Frohardt <zfrohardt@gmail.com>
2021-05-10 16:26:55 -0700

    made initial GameSetup component

--- src/components/GameSetup.js
+++ src/components/GameSetup.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Card, Grid} from 'semantic-ui-react';
 import Robot from './Robot'
 
+const MAX_ROBOTS = 3;
+
 const GameSetup = props => {
-    console.log(props.robotChoices)
+    console.log(props.robotChoices);
+
+    const [chosenRobots, setChosenRobots] = useState([]);
+    
+    let addRobotToState = (robotIndex, chosenRobots, setChosenRobots) => {
+        if (!chosenRobots.includes(robotIndex)) {
+            let roboCopy = chosenRobots.slice(0);
+            if (chosenRobots.length >= MAX_ROBOTS) {
+                roboCopy.shift();
+            }
+            roboCopy.push(robotIndex);
+            setChosenRobots(roboCopy);
+        } else {
+            setChosenRobots(chosenRobots.filter(i => robotIndex !== i));
+        }
+    }
+
+    console.log("This is the current state:");
+    console.log(chosenRobots);
+
     return (
         <Grid>
             <Grid.Row>
@@ -11,19 +32,19 @@ const GameSetup = props => {
             </Grid.Row>
             <Grid.Row>
                 <Card.Group itemsPerRow={5}>
-                    {props.robotChoices.map(robot => getRobotSelectPanel(robot, props.abilities, ()=>true))}
+                    {props.robotChoices.map((robot, index) => getRobotSelectPanel(robot, props.abilities, chosenRobots.includes(index), () => addRobotToState(index, chosenRobots, setChosenRobots)))}
                 </Card.Group>
             </Grid.Row>
-            <Button onClick={() => props.selectCallback('dummy string')} disabled={false} color="olive" content='Find Glory on the Battlefield!'/>
+            <Button onClick={() => props.selectCallback(props.robotChoices.filter((robot, index) => chosenRobots.includes(index)))} disabled={chosenRobots.length !== MAX_ROBOTS} color="olive" content='Find Glory on the Battlefield!'/>
         </Grid>
         
     );
 }
 
-const getRobotSelectPanel = (robot, abilities, select) => {
+const getRobotSelectPanel = (robot, abilities, selected, addRobot) => {
     return (
         <Card>
-            <Button content="Select"/>
+            <Button color={(selected)? "green" : "blue"} content="Select" onClick={() => addRobot()} />
             <Robot {...robot} abilities={getAbilities(robot, abilities)}/>
         </Card>
     )
