db0d6ab5bdc21d804120772ee85f263774c41080
Zach Frohardt <zfrohardt@gmail.com>
2021-05-10 14:34:42 -0700

    initial attempt at filling out game logic in Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,34 +1,58 @@
 import {React, Component} from 'react';
 import SeedRandom from 'seedrandom';
+import GameSetup from './GameSetup';
+import KillKittens from './KillKittens'
 
 export default class Game extends Component {
     constructor(props) {
         super(props);
 
-        let seedrandom = require('seedrandom');
-        let rng = seedrandom(props.seed);
+        let rng = SeedRandom(props.seed);
 
         this.state = {
-            rng: rng,
+            captainSeed: rng(),
+            randomRobotSeed: rng(),
+            warriors: [],
+            setup: true,
         }
     }
 
     render() {
+        console.log(this.getNumberFromRange(this.state.captainSeed, this.props.captains.length));
+        console.log(this.getRandomRobots());
         return (
-            <div>
-                This is where we build all of our game logic: {this.props.seed} <br />
-                This is a list of 10 random numbers. They should be the same if you provide the same URL. They should be different with a different URL. You get the idea.<br/>
-                {this.test()}
-            </div>
+            (this.state.setup)? 
+                <GameSetup captain={this.getCaptain()} robotChoices={this.getRandomRobots()} selectCallback={this.selectWarriors} /> :
+                <KillKittens warriors={this.state.warriors} captain={this.getCaptain()} />
         );
     }
 
-    test() {
-        let randomSet = [];
-        for (let i = 0; i < 10; i++) {
-            randomSet.push(<p>{this.state.rng()}</p>);
-        }
+    selectWarriors(robots) {
+        this.setState({
+            setup: false,
+            warriors: robots,
+        })
+    }
 
-        return randomSet
+    getRandomRobots() {
+        let rng = SeedRandom(this.state.randomRobotSeed);
+        let randomRobots = [];
+
+        for(let i = 0; i < 5; i++) {
+            randomRobots.push(this.getIndexFromRange(rng(), this.props.robots));
+        }
+        return randomRobots;
+    }
+
+    getCaptain() {
+        return this.getIndexFromRange(this.state.captainSeed, this.props.captains);
+    }
+
+    getIndexFromRange(rand, arr) {
+        return arr[this.getNumberFromRange(rand, arr.length)];
+    }
+
+    getNumberFromRange(rand, upperBound) {
+        return Math.floor(rand * upperBound);
     }
 }
