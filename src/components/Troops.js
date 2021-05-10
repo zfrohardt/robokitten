ce8a85870ef1368d5164385e6fe4e3eb807498d0
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 23:13:15 -0700

    added first draft of troop viewer

--- src/components/Troops.js
+++ src/components/Troops.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Card} from 'semantic-ui-react';
+import Robot from './Robot';
 
 const Troops = (props) => {
     return (
-        <div>
-            This is where you can browse our troops.
-        </div>
+        <Card.Group>
+            {props.robots.map(makeRobotCard)}
+        </Card.Group>
+    );
+}
+
+const makeRobotCard = robot => {
+    return (
+        <Robot {...robot} img={`https://robohash.org/ModelNumber${robot.modelNumber}.png`} name={`Robot Model ${robot.modelNumber}`} />
     );
 }
 
