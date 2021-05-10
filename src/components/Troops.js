da265350d1bbf84f8327b2a63d08891954f12d21
Zach Frohardt <zfrohardt@gmail.com>
2021-05-10 09:57:33 -0700

    added filtering and sort options

--- src/components/Troops.js
+++ src/components/Troops.js
@@ -1,14 +1,83 @@
 import React from 'react'
-import {Card} from 'semantic-ui-react';
+import {Dropdown, Container, Card, Menu} from 'semantic-ui-react';
 import Robot from './Robot';
 
 const Troops = (props) => {
     return (
-        <Card.Group>
-            {props.robots.map(robot => makeRobotCard(robot, props.abilities))}
-        </Card.Group>
+        <React.Fragment>
+            <Container textAlign='center' >
+                <Menu secondary compact>
+                    <Menu.Item>
+                        <Dropdown multiple selection options={classOptions} placeholder="All" onChange={handleChange} />
+                    </Menu.Item>
+                </Menu>
+            </Container>
+            <Card.Group>
+                {props.robots.map(robot => makeRobotCard(robot, props.abilities))}
+            </Card.Group>
+        </React.Fragment>
     );
 }
+
+const handleChange = (event, { value }) => {
+    console.log("This is firing!");
+    console.log(event);
+    console.log(value);
+}
+
+const sortOptions = [
+    {
+        key: 1,
+        text: "Damage",
+        value: "damage"
+    },
+    {
+        key: 2,
+        text: "Health",
+        value: "health"
+    },
+    {
+        key: 3,
+        text: "Defense",
+        value: "defense",
+    }
+]
+
+const classOptions = [
+    {
+        key: 1,
+        text: "Attacker",
+        value: "Attacker"
+    },
+    {
+        key: 2,
+        text: "Defender",
+        value: "Defender"
+    },
+    {
+        key: 3,
+        text: "Healer",
+        value: "Healer",
+    }
+]
+
+const typeOptions = [
+    {
+        key: 1,
+        text: "Fire",
+        value: "Fire",
+    },
+    {
+        key: 2,
+        text: "Lightning",
+        value: "Lightning",
+    },
+    {
+        key: 3,
+        text: "Bullet",
+        value: "Bullet",
+    }
+]
 
 const makeRobotCard = (robot, abilities) => {
     // TODO: Sort by the order of ids in the db? maybe this is a non issue
