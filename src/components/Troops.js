00b4dff7151082e48712bebe65ade09b71283f63
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 23:42:51 -0700

    finished implementing the initial attempt at the Troops component

--- src/components/Troops.js
+++ src/components/Troops.js
@@ -5,14 +5,16 @@ import Robot from './Robot';
 const Troops = (props) => {
     return (
         <Card.Group>
-            {props.robots.map(makeRobotCard)}
+            {props.robots.map(robot => makeRobotCard(robot, props.abilities))}
         </Card.Group>
     );
 }
 
-const makeRobotCard = robot => {
+const makeRobotCard = (robot, abilities) => {
+    // TODO: Sort by the order of ids in the db? maybe this is a non issue
+    let specificAbilities = abilities.filter(ability => robot.abilities.includes(ability.id));
     return (
-        <Robot {...robot} img={`https://robohash.org/ModelNumber${robot.modelNumber}.png`} name={`Robot Model ${robot.modelNumber}`} />
+        <Robot {...robot} img={`https://robohash.org/ModelNumber${robot.modelNumber}.png`} name={`Robot Model ${robot.modelNumber}`} attributes={specificAbilities} />
     );
 }
 
