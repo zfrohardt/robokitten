ed2ff30a53ac56c60f1349b515cf31c6448f56be
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 21:05:01 -0700

    added Enemies component

---
+++ src/components/Enemies.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Enemies = props => {
+    return (
+        <div>
+            This where you can browse the enemy kittens
+        </div>
+    );
+}
+
+export default Enemies;
