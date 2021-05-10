d9c23e6aed6ebc997be19356db88f8558b414e66
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 17:05:26 -0700

    fixed SplashScreen to use React Router's Link element to avoid going to the server when we load a new page

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {Button, Container, Header} from 'semantic-ui-react';
 
 const SplashScreen = props => (
     <Container text>
         <Header as='h1'>Welcome to our game!</Header>
         <Button.Group vertical labeled icon>
-            <Button as='a' href='/play' icon="play" content="Exterminate the Kittens!" color="green" /> {/* Start the game */}
-            <Button as='a' href='/troops' icon="factory" content="Examine the Troops!" color="yellow" /> {/* Look at the list of robots */}
-            <Button as='a' href='/statistics' icon="hand victory" content="Admire our Victories!" color="yellow" /> {/* Look at high scores */}
+            <Button as={Link} to="/play" icon="play" content="Exterminate the Kittens!" color="green" /> {/* Start the game */}
+            <Button as={Link} to='/troops' icon="factory" content="Examine the Troops!" color="yellow" /> {/* Look at the list of robots */}
+            <Button as={Link} to='/statistics' icon="hand victory" content="Admire our Victories!" color="yellow" /> {/* Look at high scores */}
         </Button.Group>
     </Container>
 )
