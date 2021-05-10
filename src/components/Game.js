dd67fa2377c0b82ae93876edc7556d024a78f168
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 20:32:33 -0700

    added seedable rng to Game component derived from the seed prop

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,36 @@
 import {React, Component} from 'react';
+import SeedRandom from 'seedrandom';
 
 export default class Game extends Component {
-    constructor() {
-        super();
-        this.state = {
+    constructor(props) {
+        super(props);
 
+        let seedrandom = require('seedrandom');
+        let rng = seedrandom(props.seed);
+
+        this.state = {
+            rng: rng,
         }
     }
 
     render() {
+        console.log(this.state.seed);
+
         return (
             <div>
-                This is where we build all of our game logic
+                This is where we build all of our game logic: {this.props.seed} <br />
+                This is a list of 10 random numbers. They should be the same if you provide the same URL. They should be different with a different URL. You get the idea.<br/>
+                {this.test()}
             </div>
         );
+    }
+
+    test() {
+        let randomSet = [];
+        for (let i = 0; i < 10; i++) {
+            randomSet.push(<p>{this.state.rng()}</p>);
+        }
+
+        return randomSet
     }
 }
