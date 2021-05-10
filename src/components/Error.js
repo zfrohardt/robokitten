e7b4154240543e4099038c6b0a5e4e2a94c03811
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 17:48:50 -0700

    added Error component

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = props => {
+    return (
+        <div>
+            Well, this is embarassing: 404 <br />
+            Really, you aren't supposed to see this page. Sorry about that.
+        </div>
+    );
+}
+
+export default Error;
