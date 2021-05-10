23fc2b618be96f8cf66df0632f222a7433940099
Zach Frohardt <zfrohardt@gmail.com>
2021-05-09 21:06:50 -0700

    added enemies to SplashScreen

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -5,10 +5,11 @@ import {Button, Container, Header} from 'semantic-ui-react';
 const SplashScreen = props => (
     <Container text>
         <Header as='h1'>Welcome to our game!</Header>
-        <Button.Group vertical labeled icon>
+        <Button.Group vertical labeled icon size="massive">
             <Button as={Link} to="/play" icon="play" content="Exterminate the Kittens!" color="green" /> {/* Start the game */}
-            <Button as={Link} to='/troops' icon="factory" content="Examine the Troops!" color="yellow" /> {/* Look at the list of robots */}
-            <Button as={Link} to='/statistics' icon="hand victory" content="Admire our Victories!" color="yellow" /> {/* Look at high scores */}
+            <Button as={Link} to='/troops' icon="factory" content="Examine our Troops!" color="yellow" /> {/* Look at the list of robots */}
+            <Button as={Link} to="/enemies" icon="paw" content="Study the Enemy!" color="yellow" /> {/* Look at the list of kittens */}
+            <Button as={Link} to='/statistics' icon="flag" content="Admire our Victories!" color="yellow" /> {/* Look at high scores */}
         </Button.Group>
     </Container>
 )
