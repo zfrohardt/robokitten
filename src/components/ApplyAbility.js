a9a4541a113825662ec487c40acaff39e2bb6b53
Zach Frohardt <zfrohardt@gmail.com>
2021-05-11 13:56:32 -0700

    added functions to some ability entries

--- src/components/ApplyAbility.js
+++ src/components/ApplyAbility.js
@@ -5,19 +5,19 @@ const applyAbility = (abilityId, state, self, target) => {
 }
 
 const abilityLookup = {
-    1 : (state, self, target) => true,
-    2 : (state, self, target) => true,
-    3 : (state, self, target) => true,
-    4 : (state, self, target) => true,
-    5 : (state, self, target) => true,
+    1 : (state, self, target) => true, // redirect
+    2 : (state, self, target) => {state.team = state.team.map(warrior => warrior.defense = warrior.defense + 1)},
+    3 : (state, self, target) => {state.team = state.team.map(warrior => warrior.defense = warrior.defense + 2)},
+    4 : (state, self, target) => true, // redirect
+    5 : (state, self, target) => {state.team = state.team.map(warrior => warrior.defense = warrior.defense * 2)},
     6 : (state, self, target) => false,
-    7 : (state, self, target) => true,
-    8 : (state, self, target) => true,
-    9 : (state, self, target) => true,
+    7 : (state, self, target) => {state.team = state.team.map(warrior => warrior.health = cap(warrior.health + 4, warrior.maxHealth))},
+    8 : (state, self, target) => {state.team = state.team.map(warrior => warrior.health = cap(warrior.health + 2, warrior.health))},
+    9 : (state, self, target) => {state.team = state.team.map(warrior => warrior.damage = warrior.damage + 2)},
     10: (state, self, target) => true,
     11: (state, self, target) => false,
     12: (state, self, target) => false,
-    13: (state, self, target) => true,
+    13: (state, self, target) => {state.team = state.team.map(warrior => warrior.damage = warrior.damage + 3)},
     14: (state, self, target) => false,
     15: (state, self, target) => false,
     16: (state, self, target) => false,
@@ -35,6 +35,10 @@ const abilityLookup = {
     28: (state, self, target) => false,
     29: (state, self, target) => false,
     30: (state, self, target) => false,
+}
+
+const cap = (val, cap) => {
+    return (val < cap)? val : cap;
 }
 
 export default applyAbility;
