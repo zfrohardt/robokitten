b6b34f1adafff4773101271e99fdb230a0c2775e
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-08-23 14:42:08 -0700

    abilities are now sorted before leaving the backend

--- backend/controllers/kittens.js
+++ backend/controllers/kittens.js
@@ -2,11 +2,22 @@ import mongoose from "mongoose";
 import Kitten from '../models/kitten.js'
 import Ability from '../models/ability.js'
 
+let abilitySort = (x, y) => {
+    if (x.passive === y.passive) {
+        return (x.name < y.name)? -1 : 1;
+    }
+    return (x.passive)? 1 : -1;
+}
+
 export const getAllKittens = async (req, res) => {
     try {
         const kittens = await Kitten.find().populate("abilityIds")
+        const kittensSortedAbilities = kittens.map(kitten => {
+            kitten.abilityIds.sort(abilitySort)
+            return kitten
+        })
 
-        res.status(200).json(kittens)
+        res.status(200).json(kittensSortedAbilities)
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
@@ -20,5 +31,6 @@ export const getKitten = async (req, res) => {
     }
 
     const kitten = await Kitten.findById(_id).populate("abilityIds")
+    kitten.abilityIds.sort(abilitySort)
     res.json(kitten)
 }
--- backend/controllers/robots.js
+++ backend/controllers/robots.js
@@ -2,11 +2,22 @@ import mongoose from "mongoose";
 import Robot from '../models/robot.js'
 import Ability from '../models/ability.js'
 
+let abilitySort = (x, y) => {
+    if (x.passive === y.passive) {
+        return (x.name < y.name)? -1 : 1;
+    }
+    return (x.passive)? 1 : -1;
+}
+
 export const getAllRobots = async (req, res) => {
     try {
         const robots = await Robot.find().populate("abilityIds")
+        const robotsSortedAbilities = robots.map(robot => {
+            robot.abilityIds.sort(abilitySort)
+            return robot
+        })
 
-        res.status(200).json(robots)
+        res.status(200).json(robotsSortedAbilities)
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
@@ -20,6 +31,6 @@ export const getRobot = async (req, res) => {
     }
 
     const robot = await Robot.findById(_id).populate("abilityIds")
-
+    robot.abilityIds.sort(abilitySort)
     res.json(robot)
 }
